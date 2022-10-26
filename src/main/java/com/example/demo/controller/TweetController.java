97851d9bcb5a36dc3a7cff9f338e9a2907709782
roiaki <roitoitoitoi@yahoo.co.jp>
2022-10-26 21:55:25 +0900

    更新処理の実装

--- src/main/java/com/example/demo/controller/TweetController.java
+++ src/main/java/com/example/demo/controller/TweetController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.model.Tweet;
@@ -24,6 +25,12 @@ public class TweetController {
         this.repository = repository;
     }
 
+    /**
+     * ツイート一覧表示
+     * @param tweet
+     * @param model
+     * @return　ツイート一覧画面
+     */
     @GetMapping("/")
     public String showList(@ModelAttribute Tweet tweet, Model model) {
 	model.addAttribute("tweets",
@@ -31,6 +38,13 @@ public class TweetController {
 	return "index";
     }
 
+    /**
+     * ツイート保存
+     * @param tweet
+     * @param result
+     * @param model
+     * @return　一覧画面へリダイレクト
+     */
     @PostMapping("/add")
     public String addTweet(@Validated @ModelAttribute Tweet tweet, BindingResult result, Model model) {
 	model.addAttribute("tweet", repository.findAll());
@@ -40,6 +54,35 @@ public class TweetController {
 	repository.save(tweet);
 
 	return "redirect:/";
+    }
+
+    /**
+     * TWEET編集画面を表示
+     * @param id ツイートID
+     * @param model Model
+     * @return ツイート編集画面
+     */
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) {
+	model.addAttribute("tweet", repository.findById(id));
+	return "edit";
+    }
+
+    /**
+     * TWEET更新処理
+     * @param model Model
+     * @param tweet Tweet
+     * @return ツイート一覧画面
+     */
+    @PostMapping("/update")
+    public String update(@ModelAttribute Tweet tweet, Model model) {
+
+	repository.save(tweet);
+
+	model.addAttribute("tweets",
+		repository.findAll(Sort.by(Sort.Direction.DESC, "id")));
+
+	return "index";
     }
 }
 
